d73b23c7fbcab4127c4c3c5d6b306a7d738d81da
Dan Payne <dpayne5532@gmail.com>
2023-01-30 12:09:45 -0700

    initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div>Fucking Header Bitches!</div>
+  )
+}
+
+export default Header
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './testimonials.css'
+
+const Testimonials = () => {
+  return (
+    <div>Testicles</div>
+  )
+}
+
+export default Testimonials
