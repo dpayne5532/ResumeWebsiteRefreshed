7deed48772d1c4047f77d3cd232e65c0fc7dc2a8
Dan Payne <dan@danpayne.info>
2026-02-05 09:21:58 -0700

    update portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -44,7 +44,6 @@ const Portfolio = () => {
           <h3>Local LLM Platform - Node.js | Azure | Ollama</h3>
           <p className="portfolio__item-desc">On-premises LLM processing platform using Ollama and RAG, keeps sensitive data entirely local with zero cloud exposure.</p>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/dpayne5532/localLLM-movie-recs" className='btn' target="_blank" rel="noreferrer">GitHub</a>
           <a href="https://www.youtube.com/watch?v=9h-7YfVkZEk" className='btn btn-primary' target="_blank" rel="noreferrer">Watch Demo</a>
           <a href="https://catalystsolutions-my.sharepoint.com/:p:/p/dan_payne/IQBnEG7bUUvbSIXf5lFRNmoZAT69lVaOC3yoS9UoAqfxh6E?rtime=y25WMtFk3kg" className='btn btn-primary' target="_blank" rel="noreferrer">PowerPoint Deck</a>
           </div>
